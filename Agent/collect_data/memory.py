2ed7ce80d6f1f59f42df1235a8b1d5f603299c6a
wzj <wzj@hadoop-server.(none)>
2015-11-05 10:58:13 +0800

    memory upate

--- Agent/collect_data/memory.py
+++ Agent/collect_data/memory.py
@@ -6,5 +6,9 @@ def get_mem_rate():
     with open("/proc/meminfo", "r") as f:
         mem_total = float(f.readline().split()[1])
         mem_free = float(f.readline().split()[1])
-        mem_used_rate = (mem_total - mem_free) / mem_total
+        mem_used_rate = float("%0.3f" % ((mem_total - mem_free) / mem_total))
         return mem_used_rate
+
+if __name__ == "__main__":
+    result = get_mem_rate()
+    print result
