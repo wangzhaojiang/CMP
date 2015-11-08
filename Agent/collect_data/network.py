0b430db7a12eff0f7fa13f5e6969a6c4937f07d6
wzj <wzj@hadoop-server.(none)>
2015-11-08 11:26:49 +0800

    update

--- Agent/collect_data/network.py
+++ Agent/collect_data/network.py
@@ -26,10 +26,15 @@ def caculate(data_old, data_new):
     global network_ti
     net = {}
     for key in data_old.iterkeys():
-        net[key] = (
-            float("%0.3f" % (float(data_new[key][0]) - float(data_old[key][0]) / network_ti)),
-            float("%0.3f" % (float(data_new[key][8]) - float(data_old[key][8]) / network_ti))
-        )
+       # net[key] = (
+       #     # 此处居然转化不了精度！！！
+       #     round((float(data_new[key][0]) - float(data_old[key][0])) / network_ti, 1),
+       #     round((float(data_new[key][8]) - float(data_old[key][8])) / network_ti, 1)
+       # )
+	rec = round((float(data_new[key][0]) - float(data_old[key][0])) / network_ti, 1)
+	tra = round((float(data_new[key][8]) - float(data_old[key][8])) / network_ti, 1)
+	net[key] = (rec, tra)
+	print rec,tra
 
     return net
 
