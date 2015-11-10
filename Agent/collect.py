fc0d3bacb23d12c9f482592ceae023fdb073a350
staying <wangzhaojiang2013@gmail.com>
2015-11-10 20:07:29 +0800

    update
    update

--- Agent/collect.py
+++ Agent/collect.py
@@ -3,4 +3,13 @@
 import sys
 sys.path.append("./collect_data")
 from collect_data import cpu
+<<<<<<< Updated upstream
 cpu.get_cpu_rate()
+=======
+from collect_data import diskio
+from collect_data import memory
+from collect_data import network
+
+
+cput.
+>>>>>>> Stashed changes
