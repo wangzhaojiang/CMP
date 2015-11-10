60455f39231bc39b69b849a04a713b25f7c09d55
staying <wangzhaojiang2013@gmail.com>
2015-11-10 19:50:45 +0800

    update
    update

---
+++ Agent/collect.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+# __author__ = 'JakeyWang'
+from collect_data import cpu
+from collect_data import diskio
+from collect_data import memory
+from collect_data import network
+
+
+cpu.get_cpu_rate()
