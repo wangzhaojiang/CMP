24172489175a0b934c21fd5bc3f165279da86fa4
wzj <wzj@hadoop-server.(none)>
2015-11-10 20:04:04 +0800

    update

--- Agent/collect.py
+++ Agent/collect.py
@@ -1,9 +1,6 @@
 # coding=utf-8
 # __author__ = 'JakeyWang'
+import sys
+sys.path.append("./collect_data")
 from collect_data import cpu
-from collect_data import diskio
-from collect_data import memory
-from collect_data import network
-
-
 cpu.get_cpu_rate()
