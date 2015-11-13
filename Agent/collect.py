b3b276acf0e9533e5b027d95d9e88d5deab7c622
staying <wangzhaojiang2013@gmail.com>
2015-11-13 08:35:47 +0800

    update
    update

--- Agent/collect.py
+++ Agent/collect.py
@@ -1,30 +1,48 @@
 # coding=utf-8
 # __author__ = 'JakeyWang'
 import sys
+import pika
+import json
 from utils import *
 
 
-def data_tran(data):
-    
+def data_tran(message):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+            host='10.0.0.100'))
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange='logs',
+                             type='fanout')
+
+    channel.basic_publish(exchange='logs',
+                          routing_key='',
+                          body=message)
+    print " [x] Sent %r" % (message,)
+    connection.close()
+
 
 def execmd(cmd):
-    global data
+    result = {}
+    result['type'] = cmd
+    # 数据统一用json串发送
     try:
         if cmd == "cpu":
-            data[cmd] = get_cpu_rate()
+            result['data'] = get_cpu_rate()
         elif cmd == "memory":
-            data[cmd] = get_mem_rate()
+            result['data'] = get_mem_rate()
         elif cmd == "diskio":
-            data[cmd] = get_diskio_rate()
+            result['data'] = get_diskio_rate()
         elif cmd == "network":
-            print get_net_rate()
+            result['data'] = get_net_rate()
     except Exception, e:
         pass
         print e
         # todo: Log
 
+    return result
 
 
 if __name__ == "__main__":
     if sys.argv[1]:
-        category = sys.argv[1]
+        result = execmd(sys.argv[1])
+        data_tran(json.dumps(result))
