a5ee537b667858d9de6773fd37b053bf36cc07ca
staying <wangzhaojiang2013@gmail.com>
2015-11-26 22:12:19 +0800

    admin
    admin add list_display

--- home_app/admin.py
+++ home_app/admin.py
@@ -9,7 +9,12 @@ class HostInfoAdmin(admin.ModelAdmin):
 
 @admin.register(Monitoring_data)
 class Monitoring_dataAdmin(admin.ModelAdmin):
-    list_display = ('data_date', 'hostname', 'type')
+    list_display = ('data_date', 'get_hostname', 'type')
+
+    def get_hostname(self, obj):
+        return obj.hostname.hostname
+    get_hostname.short_description = 'hostname'
+    get_hostname.admin_order_field = 'hostinfo_hostname'
 
 # admin.site.register(HostInfo)
 # admin.site.register(Monitoring_data)
